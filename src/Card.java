99041a4a3ab06b97ec9015ebd64d42be3e785a17
ryanfarokhzad <ryan_farokhzad25@milton.edu>
2023-06-02 10:18:09 -0400

    I added all of the if statements for the classes

--- src/Card.java
+++ src/Card.java
@@ -18,6 +18,46 @@ public class Card {
         if(pSuit == 3){
             suit ="hearts";
         }
+        if(pName == 0){
+            name = "Ace";
+        }
+        if(pName == 1){
+            name = "two";
+        }
+        if(pName == 2){
+            name = "three";
+        }
+        if(pName == 3){
+            name = "four";
+        }
+        if(pName == 4){
+            name = "five";
+        }if(pName == 5){
+            name = "six";
+        }
+        if(pName == 6){
+            name = "seven";
+        }
+        if(pName == 7){
+            name = "eight";
+        }
+        if(pName == 8){
+            name = "nine";
+        }
+        if(pName == 9){
+            name = "ten";
+        }
+        if(pName == 10){
+            name = "jack";
+        }
+        if(pName == 11){
+            name = "queen";
+        }
+        if(pName == 12){
+            name = "king";
+        }
+
+
 
     }
 }
